9445962e05c01f9ff0fd392b33ae026b8329d8cf
GloiMir <108124349+GloiMir@users.noreply.github.com>
2022-06-27 15:22:57 +0200

    Update exercice1.js

--- exercice1/exercice1.js
+++ exercice1/exercice1.js
@@ -10,14 +10,18 @@ const form = document.getElementById("form")
 send.onclick = function(){
   if(prenom.value==""){
     document.getElementById("erreurPrenom").textContent="First name can not be empty";
+    prenom.style.border = "solid 1px red"
   }
   if(nom.value==""){
     document.getElementById("erreurNom").textContent="Name can not be empty";
+    nom.style.border = "solid 1px red"
   }
   if(!(/^([a-z0-9]{1,})@([a-z0-9]{1,})\.([a-z0-9]{2,5})$/.test(mail.value))){
     document.getElementById("erreurMail").textContent="Looks like this is not not mail";
+    mail.style.border = "solid 1px red"
   }
   if(mdp.value==""){
     document.getElementById("erreurPassword").textContent="Password can not be empty";
+    mdp.style.border = "solid 1px red"
   }
 }
